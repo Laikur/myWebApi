4c37f76f88e99aeebd6dd47b913cdd083edb3749
Laikur Rahman <vickyali09@gmail.com>
2023-05-14 14:35:30 +0530

    end of session 1

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myWebAPI.Data;
+using myWebAPI.Entities;
+using SQLitePCL;
+
+namespace myWebAPI.Controllers
+{
+    [ApiController]
+    [Route("mywebapi/[controller]")] // /mywebapi/Users
+    public class UsersController
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+            
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user =await _context.Users.FindAsync(id);
+            return user;
+        }
+    }
+}
